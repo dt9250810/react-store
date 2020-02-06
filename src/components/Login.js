de61b5d67299354e8c57ffa0ab31a3a86413c211
bruce <dt9250810@gmail.com>
2020-02-06 21:38:49 +0800

    feat(Login): html form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 
 class Login extends React.Component {
   render() {
-    return <p>Login Component</p>;
-    // return React.createElement('p', {className: 'login'}, 'Login Component2222');
+    return (
+      <form className="login-wrapper">
+        <div>
+          <label>Email</label>
+          <div>
+            <input type="text" placeholder="Email" />
+          </div>
+        </div>
+        <div>
+          <label>Password</label>
+          <div>
+            <input type="text" placeholder="Password" />
+          </div>
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    )
   }
 }
 
