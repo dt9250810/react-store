1c397c2ebeeef88362721d4876ae68d4342758e0
bruce <dt9250810@gmail.com>
2020-02-06 21:20:55 +0800

    hello Login

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Login extends React.Component {
+  render() {
+    return <p>Login Component</p>;
+    // return React.createElement('p', {className: 'login'}, 'Login Component2222');
+  }
+}
+
+
+export default Login;
