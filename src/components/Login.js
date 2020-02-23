649ce5868ef6fc4d2b8265bc84b53e73aa019005
bruce <dt9250810@gmail.com>
2020-02-23 13:53:40 +0800

    feat: 登入頁面

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,40 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 
+
 class Login extends React.Component {
   render() {
     return (
-      <form className="login-wrapper">
-        <div>
-          <label>Email</label>
-          <div>
-            <input type="text" placeholder="Email" />
+      <div className="login-wrapper">
+        <form className="box login-box">
+          <div class="field">
+            <p class="control has-icons-left has-icons-right">
+              <input class="input" type="email" placeholder="Email" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+              <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+              </span>
+            </p>
           </div>
-        </div>
-        <div>
-          <label>Password</label>
-          <div>
-            <input type="text" placeholder="Password" />
+          <div class="field">
+            <p class="control has-icons-left">
+              <input class="input" type="password" placeholder="Password" />
+              <span class="icon is-small is-left">
+                <i class="fas fa-lock"></i>
+              </span>
+            </p>
           </div>
-        </div>
-        <div>
-          <button>Login</button>
-        </div>
-      </form>
+          <div class="field">
+            <p class="control">
+              <button class="button is-success">
+                Login
+              </button>
+            </p>
+          </div>
+        </form>
+      </div>
     )
   }
 }
