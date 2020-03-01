da6241f8b4972e0f62dcf94c4d1d7166becf6ac4
bruce <dt9250810@gmail.com>
2020-03-01 21:11:10 +0800

    feat(axios): add axios (RESTful API)

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,106 +1,30 @@
 import React from 'react'
+import axios from 'commons/axios'
 import ToolBox from 'components/ToolBox'
 import Product from 'components/Product'
 
 class Products extends React.Component {
-  products = [
-    {
-      id: 1,
-      name: "Air Jordan 4",
-      image: "/images/1.jpg",
-      tags: "92",
-      price: 59440,
-      status: "available"
-    },
-    {
-      id: 2,
-      name: "Nike Paul George PG 3",
-      image: "/images/2.jpg",
-      tags: "25 Colors",
-      price: 53800,
-      status: "unavailable"
-    },
-    {
-      id: 3,
-      name: "Jordan Why Not Zer0.2",
-      image: "/images/3.jpg",
-      tags: "16 Color, y",
-      price: 48900,
-      status: "available"
-    },
-    {
-      id: 4,
-      name: "Nike Air Foamposite One",
-      image: "/images/4.jpg",
-      tags: "84 Colors",
-      price: 73148,
-      status: "available"
-    },
-    {
-      id: 5,
-      name: "adidas Harden Vol.3",
-      image: "/images/5.jpg",
-      tags: "34 Colors",
-      price: 46900,
-      status: "available"
-    },
-    {
-      id: 6,
-      name: "Nike Zoom Kobe 1",
-      image: "/images/6.jpg",
-      tags: "26 Colors",
-      price: 80391,
-      status: "available"
-    },
-    {
-      id: 7,
-      name: "Nike Hyperdunk X(2018)",
-      image: "/images/7.jpg",
-      tags: "",
-      price: 54600,
-      status: "available"
-    },
-    {
-      id: 8,
-      name:" Nike KD 12",
-      image: "/images/8.jpg",
-      tags: "",
-      price: 81800,
-      status: "available"
-    },
-    {
-      id: 9,
-      name: "adidas D.O.N. Issue #1",
-      price: 38800,
-      status: "available",
-      tags: "",
-      image: "/images/9.jpg",
-    },
-    {
-      id: 10,
-      name: "adidas D.O.N. Issue #1",
-      price: 48900,
-      status: "available",
-      tags: "",
-      image: "/images/10.jpg",
-    },
-    {
-      id: 11,
-      name: "Nike Kyrie Flytrap",
-      price: 34400,
-      status: "available",
-      tags: "",
-      image: "/images/11.jpg",
-    },
-    {
-      id: 12,
-      name: "Air Jordan 3",
-      price: 44900,
-      status: "available",
-      tags: "",
-      image: "/images/12.jpg",
-    }
-  ]
+  state = {
+    products: []
+  };
+
+  componentDidMount() {
+    // fetch('http://localhost:3001/products')
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //     this.setState({  products: data })
+    //   })
+    //   .catch(err => { console.log('錯誤:', err); });
+    axios.get('/products')
+      .then(response => {
+        console.log(response.data);
+        this.setState({  products: response.data })
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  };
 
   render() {
     return (
@@ -110,7 +34,7 @@ class Products extends React.Component {
           <div className="columns is-multiline is-desktop">
 
             {
-              this.products.map(p => {
+              this.state.products.map(p => {
                 return (
                   <div className="column is-3" key={p.id}>
                     <Product product={p}/>
