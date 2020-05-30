4f8f9282b929ace8827c3b9864a0be5209b5ae32
bruce <dt9250810@gmail.com>
2020-05-30 21:56:00 +0800

    feat: CartItem.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,47 @@
-import React from 'react'
-import Header from 'components/Header'
+import React, { useState, useEffect } from 'react'
+import axios from 'commons/axios'
 import Layout from 'Layout'
+import CartItem from 'components/CartItem'
+import { formatPrice } from 'commons/extensions'
 
-const Cart = () => (
-  <Layout>
-    <div className="car-page">
-      <p className="title has-text-centered">Cart Page</p>
-    </div>
-  </Layout>
-)
-  
+const Cart = () => {
+  const [carts, setCarts] = useState([])
+
+  // 每次 render 都會執行 useEffect
+  useEffect(() => {
+    axios.get('/carts').then(res =>
+      setCarts(res.data)
+    )
+  })
+
+  const totalPrice = () => {
+    let totalPrice = carts.map(cart => cart.mount * parseInt(cart.price))
+                          .reduce((total, value) => total + value, 0)
+
+    return totalPrice
+  }
+
+  return (
+    <Layout>
+      <div className="cart-page">
+        <span className="cart-title">Shopping Cart</span>
+        <div className="cart-list">
+          {
+            carts.map(
+              cart => <CartItem key={cart.id} cart={cart} />
+            )
+          }
+        </div>
+        <div className="cart-total">
+          Total:
+          <span className="total-price">
+            {formatPrice(totalPrice())}
+          </span>
+        </div>
+      </div>
+    </Layout>
+  )
+}
 
 
 export default Cart;
