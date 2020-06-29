e9c473a77612213fa6ac511f9f43b86ca883ccf3
bruce <dt9250810@gmail.com>
2020-06-29 21:07:57 +0800

    feat: { useForm } from react-hook-form, register, handleSubmit and errors

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,95 +1,88 @@
 import React, { Fragment } from 'react'
-import ReactDOM from 'react-dom'
+import { useForm } from 'react-hook-form'
 
+export default function Login(props) {
 
-class Login extends React.Component {
+  const { register, handleSubmit, errors } = useForm();
 
-  // 受控組件
-  state = {
-    email: '',
-    password: ''
-  }
-
-  // State
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     isLike: false
-  //   }
-  // }
-
-  handleSubmit = event => {
-    // 1. 阻止默認事件行為(跳轉)
-    event.preventDefault()
-
+  const onSubmit = data => {
     // 2. 獲取 form 數據
     // const formData = {
     //   email: this.emailRef.current.value, 
     //   password: this.passwordRef.current.value
     // }
-    console.log(this.state)
+    console.log(data)
     // 3. 處理 登入 邏輯
 
     // 4. 點擊後跳轉至首頁
-    this.props.history.push('/')
+    // this.props.history.push('/')
   }
+  console.log(errors)
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-
-  render() {
-    return (
-      <div className="login-wrapper">
-        <form className="box login-box" onSubmit={this.handleSubmit}>
-          <div class="field">
-            <p class="control has-icons-left has-icons-right">
+  return (
+    <div className="login-wrapper">
+      <form className="box login-box" onSubmit={handleSubmit(onSubmit)}>
+        <div className="field">
+          <p className="control has-icons-left has-icons-right">
               <input 
-                class="input" 
+                className={`input ${errors.email && 'is-danger'}`} 
                 type="email" 
                 placeholder="Email" 
                 name="email"
-                value={this.state.email} 
-                onChange={this.handleChange}
+                ref={register({
+                  required: 'email is required',
+                  pattern: {
+                    value: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/,
+                    message: 'invalid email format'
+                  },
+                  minLength: {
+                    value: 6,
+                    message: 'can not be less than 6 words'
+                  }
+                })}
               />
-              <span class="icon is-small is-left">
-                <i class="fas fa-envelope"></i>
+              {errors.email && (
+                <span className="helper has-text-danger">{errors.email.message}</span>
+              )}
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
               </span>
-              <span class="icon is-small is-right">
-                <i class="fas fa-check"></i>
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
               </span>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control has-icons-left">
-              <input 
-                class="input" 
-                type="password" 
-                placeholder="Password" 
-                name="password"
-                value={this.state.password}
-                onChange={this.handleChange}
-              />
-              <span class="icon is-small is-left">
-                <i class="fas fa-lock"></i>
-              </span>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <button class="button is-success is-fullwidth">
-                Login
-              </button>
-            </p>
-          </div>
-        </form>
-      </div>
-    )
-  }
+          </p>
+        </div>
+        <div className="field">
+          <p className="control has-icons-left">
+            <input 
+              className={`input ${errors.password && 'is-danger'}`} 
+              type="password" 
+              placeholder="Password" 
+              name="password"
+              ref={register({
+                required: 'password is required',
+                minLength: {
+                  value: 6,
+                  message: 'can not be less than 6 words'
+                }
+              })}
+            />
+            {errors.password && (
+              <span className="helper has-text-danger">{errors.password.message}</span>
+            )}
+            <span className="icon is-small is-left">
+              <i className="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <div className="field">
+          <p className="control">
+            <button className="button is-success is-fullwidth">
+              Login
+            </button>
+          </p>
+        </div>
+      </form>
+    </div>
+  )
 }
-
-
-export default Login;
